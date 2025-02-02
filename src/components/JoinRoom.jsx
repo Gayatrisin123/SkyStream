74155215ba37b22498a42cdf5313f355f475e07c
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-03 01:19:12 +0530

    Adding Fallback Command for Exceptions

--- src/components/JoinRoom.jsx
+++ src/components/JoinRoom.jsx
@@ -2,11 +2,30 @@ import { Suspense, lazy } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 const Joining = lazy(() => import("./connection/Joining"));
-const FallbackUI = () => <div>Loading...</div>;
+const FallbackUI = () => (
+  <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center space-x-4">
+      <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-blue-600"></div>
+      <p className="text-lg font-semibold text-gray-700">Loading...</p>
+    </div>
+  </div>
+);
+
 const ErrorFallback = ({ error, resetErrorBoundary }) => (
-  <div>
-    <p>Something went wrong: {error.message}</p>
-    <button onClick={resetErrorBoundary}>Try Again</button>
+  <div className="flex flex-col items-center justify-center p-4 rounded-lg shadow-md">
+    <p className="text-lg text-red-600 font-semibold mb-4">
+      Something went wrong: {error.message}
+    </p>
+    <button
+      type="button"
+      onClick={() => {
+        resetErrorBoundary();
+        window.location.href = "/";
+      }}
+      className="px-6 py-2 bg-red-600 text-white font-semibold rounded-lg shadow hover:bg-red-700 transition duration-200"
+    >
+      Try Again
+    </button>
   </div>
 );
 
