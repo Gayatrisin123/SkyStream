f0d98a8ddee11be8f6affcb5957e6fcdb43e800c
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-05 17:05:55 +0530

    Framer Motion - Home Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,16 @@ import {
 } from "../@/components/ui/card";
 import { Monitor, Users } from "lucide-react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const textVariant = (delay) => ({
+  hidden: { y: -50, opacity: 0 },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: { type: "spring", duration: 1.25, delay: delay },
+  },
+});
 
 export default function Home() {
   return (
@@ -17,6 +27,11 @@ export default function Home() {
 
       <div className="container mx-auto flex flex-col items-center text-center">
         <div className="max-w-4xl space-y-6">
+          <motion.div
+            variants={textVariant(0.3)}
+            initial="hidden"
+            animate="show"
+          >
           <h1 className="text-5xl sm:text-6xl font-extrabold text-white tracking-tight">
             Share Your Screen,{" "}
             <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-blue-500">
@@ -24,13 +39,25 @@ export default function Home() {
             </span>{" "}
             in Real-Time
           </h1>
+          </motion.div>
+          <motion.div
+            variants={textVariant(0.8)}
+            initial="hidden"
+            animate="show"
+          >
           <p className="text-lg md:text-xl text-gray-400">
             Collaborate seamlessly with our powerful screen sharing and live
             chat platform.
           </p>
+          </motion.div>
         </div>
 
         <div className="grid md:grid-cols-2 gap-8 mt-14 w-full max-w-3xl">
+          <motion.div
+            variants={textVariant(1.4)}
+            initial="hidden"
+            animate="show"
+          >
           <Card
             className="relative bg-white/10 backdrop-blur-lg border border-transparent hover:border-gradient-to-r hover:from-purple-500 hover:to-blue-500 rounded-xl shadow-lg"
             style={{
@@ -49,13 +76,19 @@ export default function Home() {
             </CardHeader>
             <CardContent>
               <Link to="/host">
-                <Button className="w-full bg-gradient-to-r from-purple-500 to-blue-500 hover:shadow-lg hover:shadow-purple-500/50 text-white transition-transform transform hover:scale-105 rounded-lg">
+                <Button className="w-full bg-gradient-to-r border-2 border-gray-500 text-white hover:shadow-lg hover:shadow-purple-500/50 text-white transition-transform transform hover:scale-105 rounded-lg">
                   Create Room
                 </Button>
               </Link>
             </CardContent>
           </Card>
+        </motion.div>
 
+        <motion.div
+          variants={textVariant(2.1)}
+          initial="hidden"
+          animate="show"
+        >
           <Card
             className="relative bg-white/10 backdrop-blur-lg border border-transparent hover:border-gradient-to-r hover:from-blue-500 hover:to-green-400 rounded-xl shadow-lg"
             style={{
@@ -76,13 +109,14 @@ export default function Home() {
               <Link to="/join">
                 <Button
                   variant="outline"
-                  className="w-full border-2 border-gray-500 text-white bg-gradient-to-r hover:from-gray-800 hover:to-gray-600 hover:text-gray-300 hover:shadow-lg transition-transform transform hover:scale-105 rounded-lg"
+                  className="w-full border-2 border-gray-500 text-white bg-gradient-to-r hover:text-gray-300 hover:shadow-lg transition-transform transform hover:scale-105 rounded-lg"
                 >
                   Join Room
                 </Button>
               </Link>
             </CardContent>
           </Card>
+        </motion.div>
         </div>
       </div>
     </section>
