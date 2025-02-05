3720c0b1358f768843761295f977f0777afaa4ac
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-02-05 16:15:07 +0530

    Adding Framer Motion - Contact Us

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+const textVariant = (delay) => ({
+  hidden: { y: -50, opacity: 0 },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: { type: "spring", duration: 1.25, delay: delay },
+  },
+});
 
 export default function HelpCenter() {
   const [isLoading, setIsLoading] = useState(true);
@@ -37,13 +47,31 @@ export default function HelpCenter() {
       <div className="absolute bottom-10 right-10 w-20 h-20 bg-purple-600 rounded-full blur-2xl opacity-40 animate-pulse"></div>
 
       <div className="text-center mb-2">
+        <motion.div
+          variants={textVariant(0.4)}
+          initial="hidden"
+          animate="show"
+        >
         <h2 className="text-5xl md:text-6xl font-extrabold text-white leading-tight tracking-tight">
           Contact Form
         </h2>
+        </motion.div>
+        <motion.div
+          variants={textVariant(1)}
+          initial="hidden"
+          animate="show"
+        >
         <p className="text-lg text-white mt-4 max-w-2xl mx-auto opacity-90">
           Easily connect with us through our contact page. Submit your details, and we’ll respond promptly to assist you.
         </p>
+        </motion.div>
+        <motion.div
+          variants={textVariant(1.4)}
+          initial="hidden"
+          animate="show"
+        >
         <div className="mt-4 h-1 w-20 mx-auto bg-gradient-to-r from-blue-500 to-purple-600 rounded-full"></div>
+        </motion.div>
       </div>
 
       {isLoading && (
