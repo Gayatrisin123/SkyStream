a3ffbb53f7596aa47e28c5380d1ea2df01868682
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-04-15 00:50:17 +0530

    Added Error 404 window

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import Layout from "./components/Layout.jsx";
-import Header from './components/Header.jsx';
+import Layout from "./components/common/Layout.jsx";
+import Header from './components/common/Header.jsx';
 import Home from './pages/Home.jsx';
 // import ChatRoom from './pages/ChatRoom.jsx';
 import ChatRoom from './components/ChatRoom/ChatRoom.jsx';
@@ -11,6 +11,7 @@ import ContactUs from './pages/ContactUs.jsx';
 import Help from './pages/HelpCenter.jsx';
 import Host from './components/HostRoom.jsx';
 import Join from './components/JoinRoom.jsx';
+import Error404 from "./components/common/Error404.jsx";
 import './App.scss';
 
 function App() {
@@ -27,6 +28,8 @@ function App() {
               <Route path="/help-center" element={<Help />} />
               <Route path="/host" element={<Host />} />
               <Route path="/join" element={<Join />} />
+              <Route path="/error404" element={<Error404 />} />
+              <Route path="*" element={<Navigate to="/error404" />} />
             </Route>
           </Routes>
         </>
