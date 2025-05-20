7470ee701d584e3d2856b7ec0a52ade76a512ca8
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-05-20 16:10:56 +0530

    Adding Home2 Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import Home1 from "../components/HomeSection/Home1";
+import Home2 from "../components/HomeSection/Home2";
+import { LampSeparator } from "../components/ui/LampSeparator";
 
 export default function Home() {
   return (
     <div className="text-center justify-center">
       <Home1 />
+      <div className="-mt-8 -mb-16">
+        <LampSeparator/>
+      </div>
+      <Home2 />
     </div>
   );
 }
